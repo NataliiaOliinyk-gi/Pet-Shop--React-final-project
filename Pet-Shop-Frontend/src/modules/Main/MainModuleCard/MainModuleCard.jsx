636fabe58d0d12cfeec070b0e8f4ddaf5afffc65
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-05-05 10:36:16 +0200

    Initial commit

--- Pet-Shop-Frontend/src/modules/Main/MainModuleCard/MainModuleCard.jsx
+++ Pet-Shop-Frontend/src/modules/Main/MainModuleCard/MainModuleCard.jsx
@@ -1,18 +1,34 @@
 
+import SectionLayout from '../../../layouts/SectionLayout/SectionLayout';
+import Carousel from '../../Carousel/Carousel';
+
 import ModuleTitle from '../../ModuleTitle/ModuleTitle';
+
+// import { Link } from 'react-router-dom';
+// import CategorieCard from '../Categories/CategorieCard/CategorieCard';
+import useFetch from '../../../hooks/useFetch';
+import { getCategoriesAll } from '../../../api/categories-api';
+// import { slugify } from '../../utils/slugify';
 
 import styles from './MainModuleCard.module.css';
 
 const MainModuleCard = () => {
 
+    const { data, loading, error } = useFetch({
+        request: getCategoriesAll,
+        initialData: [],
+    });
 
     return (
         <div>
-            <ModuleTitle
-                text="Categories"
-                name='All categories'
-                to='/categories'
-            />
+            <SectionLayout>
+                <ModuleTitle
+                    text="Categories"
+                    name='All categories'
+                    to='/categories'
+                />
+                <Carousel data={data} loading={loading} error={error} to='/categories' name='All categories' />
+            </SectionLayout>
         </div>
     )
 };
