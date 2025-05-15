e1b3e739cccacb1cc219b5a05e13d4ef7b723160
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-05-15 13:18:38 +0200

    remove AdvancedMarker from LocationMap

--- Pet-Shop-Frontend/src/shared/components/LocationMap/LocationMap.jsx
+++ Pet-Shop-Frontend/src/shared/components/LocationMap/LocationMap.jsx
@@ -1,16 +1,12 @@
-import { useState, useEffect, useCallback, memo, useRef } from 'react';
+import { useState, useEffect, useCallback, memo } from 'react';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 import Loader from '../Loader/Loader';
-
 
 const containerStyle = {
     width: '100%',
     height: '400px',
 };
-
-const libraries = ['places'];
-// const libraries = ['maps', 'marker']
 
 const LocationMap = ({ address }) => {
 
@@ -19,13 +15,10 @@ const LocationMap = ({ address }) => {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: googleMapsApiKey,
-        libraries: libraries,
-        version: 'beta'
     });
 
     const [map, setMap] = useState(null);
     const [center, setCenter] = useState(null);
-    const markerContentRef = useRef(null);
 
     // Геокодування адреси при завантаженні
     useEffect(() => {
@@ -45,21 +38,6 @@ const LocationMap = ({ address }) => {
         });
     }, [isLoaded, address]);
 
-    // Додаємо AdvancedMarkerElement вручну
-    useEffect(() => {
-        if (!map || !center || !window.google?.maps?.marker?.AdvancedMarkerElement) return;
-
-        const marker = new window.google.maps.marker.AdvancedMarkerElement({
-            map,
-            position: center,
-            content: markerContentRef.current,
-        });
-
-        return () => {
-            if (marker.map) marker.map = null; // очищення
-        };
-    }, [map, center]);
-
     const onLoad = useCallback((mapInstance) => {
         setMap(mapInstance);
     }, []);
@@ -68,49 +46,28 @@ const LocationMap = ({ address }) => {
         setMap(null);
     }, []);
 
-
-    if (!isLoaded || !center) {
-        return <p>Loading map...</p>;
-    }
+    if (!isLoaded || !center) return <Loader />
 
     return (
-
         <>
-            {
-                isLoaded ? (
-                    <div>
-                        <GoogleMap
-                            mapContainerStyle={containerStyle}
-                            center={center}
-                            zoom={15}
-                            onLoad={onLoad}
-                            onUnmount={onUnmount}
-                            options={{
-                                mapId: 'ba0e6088b35bf1d52bcedee2'
-                            }}
-                        >
-                            <Marker position={center} />
-                        </GoogleMap>
-
-                        {/* Рендеримо маркерний DOM-елемент поза картою */}
-                        {/* <div
-                            ref={markerContentRef}
-                            style={{
-                                backgroundColor: 'red',
-                                width: '20px',
-                                height: '20px',
-                                borderRadius: '50%',
-                                border: '2px solid white',
-                                display: 'none'
-                            }}
-                        /> */}
-
-                    </div>
-                ) : (
-                    // <p>Loading map...</p>
-                    <Loader />
-                )
-            }
+            {isLoaded ? (
+                <div>
+                    <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        center={center}
+                        zoom={15}
+                        onLoad={onLoad}
+                        onUnmount={onUnmount}
+                        options={{
+                            mapId: 'ba0e6088b35bf1d52bcedee2'
+                        }}
+                    >
+                        <Marker position={center} />
+                    </GoogleMap>
+                </div>
+            ) : (
+                <Loader />
+            )}
         </>
     )
 };
